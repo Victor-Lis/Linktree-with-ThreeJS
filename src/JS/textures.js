4505ea5fdd492a260064aace773b5c07974425e2
Victor-Lis <victorlisbronzo1@gmail.com>
2024-01-14 15:41:25 -0300

    v2.2
    src/imgs
    - Adicionando novas imagens
    
    src/JS/textures.js
    - Transformando as novas imagens em texturas.

--- src/JS/textures.js
+++ src/JS/textures.js
@@ -7,6 +7,9 @@ import madeiraImg from '../imgs/madeira.jpeg'
 import quartzoImg from '../imgs/quartzo.svg'
 import pedregulhoImg from '../imgs/pedregulho.jpeg'
 import ballImg from '../imgs/ball.png'
+import portalImg from '../imgs/portal.gif'
+import obsidianImg from '../imgs/obsidiana.jpeg'
+import glowstoneImg from '../imgs/glowstone.png'
 
 const textureLoader = new THREE.TextureLoader()
 
@@ -18,5 +21,20 @@ const madeira = textureLoader.load(madeiraImg)
 const quartzo = textureLoader.load(quartzoImg)
 const pedregulho = textureLoader.load(pedregulhoImg)
 const bola = textureLoader.load(ballImg)
+const portal = textureLoader.load(portalImg)
+const obsidiana = textureLoader.load(obsidianImg)
+const glowstone = textureLoader.load(glowstoneImg)
 
-export { grass, brick, linkedin, github, madeira, quartzo, pedregulho, bola }
+export { 
+    grass, 
+    brick, 
+    linkedin, 
+    github, 
+    madeira, 
+    quartzo, 
+    pedregulho, 
+    bola, 
+    portal, 
+    obsidiana,
+    glowstone
+}
