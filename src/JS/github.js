08262c8a1d18cd30193416c54e9d953afa600f2b
Victor-Lis <victorlisbronzo1@gmail.com>
2024-01-13 21:14:54 -0300

    v1.8
    Módulo responsável pela criação dos componentes relacionados ao "Linkedin".

---
+++ src/JS/github.js
@@ -0,0 +1,167 @@
+import * as THREE from 'three';
+import * as CANNON from 'cannon-es'
+import * as dat from 'dat.gui'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import {
+    scene
+} from './scene.js'
+ 
+import {
+    github
+} from './textures.js'
+
+import {
+    sphere
+} from './scenePhys.js'
+
+import { 
+    world 
+} from './scenePhys';
+
+import {
+    createWall
+} from './functions.js'
+
+const githubBaseWall1 = createWall({
+    geoWidth: 200,
+    geoHeight: 10,
+    geoDepth: 2.5,
+    bodyPositionX: 0,
+    bodyPositionZ: -100
+})
+
+const githubBaseWall1SphereContactMat = new CANNON.ContactMaterial(
+    githubBaseWall1.physMat,
+    sphere.phys,
+    {
+        restitution: 0,
+    }
+)
+
+world.addContactMaterial(githubBaseWall1SphereContactMat)
+
+const githubBaseWall2 = createWall({
+    geoWidth: 50,
+    geoHeight: 10,
+    geoDepth: 2.5,
+    bodyPositionX: 100,
+    bodyPositionZ: -75
+})
+
+const githubBaseWall2SphereContactMat = new CANNON.ContactMaterial(
+    githubBaseWall2.physMat,
+    sphere.phys,
+    {
+        restitution: 0,
+    }
+)
+
+githubBaseWall2.body.quaternion.setFromEuler(0, Math.PI * 1.5, 0);
+world.addContactMaterial(githubBaseWall2SphereContactMat)
+
+const githubBaseWall3 = createWall({
+    geoWidth: 25,
+    geoHeight: 10,
+    geoDepth: 2.5,
+    bodyPositionX: -100,
+    bodyPositionZ: -87.5
+})
+
+const githubBaseWall3SphereContactMat = new CANNON.ContactMaterial(
+    githubBaseWall3.physMat,
+    sphere.phys,
+    {
+        restitution: 0,
+    }
+)
+
+githubBaseWall3.body.quaternion.setFromEuler(0, Math.PI * 1.5, 0);
+world.addContactMaterial(githubBaseWall3SphereContactMat)
+
+const githubBaseWall4 = createWall({
+    geoWidth: 100,
+    geoHeight: 10,
+    geoDepth: 2.5,
+    bodyPositionX: -100,
+    bodyPositionZ: -50
+})
+
+const githubBaseWall4SphereContactMat = new CANNON.ContactMaterial(
+    githubBaseWall4.physMat,
+    sphere.phys,
+    {
+        restitution: 0,
+    }
+)
+
+githubBaseWall4.body.quaternion.setFromEuler(0, 0, 0);
+world.addContactMaterial(githubBaseWall4SphereContactMat)
+
+const githubBaseWall5 = createWall({
+    geoWidth: 50,
+    geoHeight: 10,
+    geoDepth: 2.5,
+    bodyPositionX: -50,
+    bodyPositionZ: -75
+})
+
+const githubBaseWall5SphereContactMat = new CANNON.ContactMaterial(
+    githubBaseWall5.physMat,
+    sphere.phys,
+    {
+        restitution: 0,
+    }
+)
+
+githubBaseWall5.body.quaternion.setFromEuler(0, Math.PI * 1.5, 0);
+world.addContactMaterial(githubBaseWall5SphereContactMat)
+
+const githubFloor = createWall({
+    geoWidth: .5,
+    geoHeight: 50,
+    geoDepth: 50,
+    bodyPositionX: -75,
+    bodyPositionY: 1,
+    bodyPositionZ: -75,
+    color: 0x202020,
+    map: github,
+})
+
+const githubFloorSphereContactMat = new CANNON.ContactMaterial(
+    githubFloor.physMat,
+    sphere.phys,
+    {
+        restitution: 0,
+    }
+)
+
+const point3Light = new THREE.PointLight(0xffffff, 12500, 300)
+point3Light.position.set(-80, 30, -80)
+scene.add(point3Light)
+
+githubFloor.body.quaternion.setFromEuler(Math.PI * 1.5, Math.PI * 1.5, 0);
+console.log(githubFloor.body)
+world.addContactMaterial(githubFloorSphereContactMat)
+
+function renderGithub(){
+    githubBaseWall1.mesh.position.copy(githubBaseWall1.body.position)
+    githubBaseWall1.mesh.quaternion.copy(githubBaseWall1.body.quaternion)
+
+    githubBaseWall2.mesh.position.copy(githubBaseWall2.body.position)
+    githubBaseWall2.mesh.quaternion.copy(githubBaseWall2.body.quaternion)
+
+    githubBaseWall3.mesh.position.copy(githubBaseWall3.body.position)
+    githubBaseWall3.mesh.quaternion.copy(githubBaseWall3.body.quaternion)
+
+    githubBaseWall4.mesh.position.copy(githubBaseWall4.body.position)
+    githubBaseWall4.mesh.quaternion.copy(githubBaseWall4.body.quaternion)
+
+    githubBaseWall5.mesh.position.copy(githubBaseWall5.body.position)
+    githubBaseWall5.mesh.quaternion.copy(githubBaseWall5.body.quaternion)
+
+    githubFloor.mesh.position.copy(githubFloor.body.position)
+    githubFloor.mesh.quaternion.copy(githubFloor.body.quaternion)
+}
+
+export { renderGithub }
